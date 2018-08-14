6d45942f68fd08b16c943782d77c0e3c094939e2
Dennis <denniskamau3@gmail.com>
2018-08-14 12:51:59 +0300

    add router resource

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const Routes = require('./recources/routes')
+
 const app = express()
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
 const port = process.env.PORT || 8080
-const router = express.Router()
 
-router.get('/', (req,res)=>{
-    res.json({message:"you hit the router get"})
-})
 
-app.get('/', (req,res)=>{
-    res.json({message: "you hit the app get"})
-})
-
-app.use('/api/v1', router)
+app.use('/api/v1', Routes)
 
 app.listen(port)
 console.log('server running on port ' + port)
