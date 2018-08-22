f48bc740dc5756c7eba41b345f068ce934f61ca9
Dennis <denniskamau3@gmail.com>
2018-08-22 17:34:03 +0300

    finish authentication

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,8 +1,7 @@
 const User = require('../models').User
 const LocalStrategy = require('passport-local').Strategy
-const passport = require('passport')
 var bCrypt = require('bcrypt-nodejs');
-const jwt = require('jsonwebtoken');
+
 
 
 
@@ -68,12 +67,12 @@ module.exports = (passport, user)=> {
                                 return done(null,false)
                             }
                             if(newUser){
-                                let token = jwt.sign({id:newUser.id},'server secret');
-                                return done(null,false,req.flash('session',token))
+                                return done(null,newUser);
                             }
                         });
                     }else {
-                        return done(null, false, req.flash('error', 'user already exists'))
+                        const info = {error:"User already exists"}
+                        return done(null, info)
                     }
                 })
 
@@ -100,20 +99,20 @@ module.exports = (passport, user)=> {
             }).then((user)=>{
             // if there are any errors, return the error before anything else
             // if no user is found, return the message
-            if (!user)
-                return done(null, false, req.flash('error', 'User does not exist')); // req.flash is the way to set flashdata using connect-flash
-
+            if (!user){
+                var info = {error:"User does not exist"}
+                return done(null, info); // req.flash is the way to set flashdata using connect-flash
+            }
             // if the user is found but the password is wrong
             let isValidPassword = (userpass,password)=>{
                 return bCrypt.compareSync(password, userpass);
             }
-            if (isValidPassword(user.password,password))
-                return done(null, false, req.flash('error', 'Invalid Password')); // create the loginMessage and save it to session as flashdata
-
+            if (!isValidPassword(user.password,password)){
+                var info = {error:'Invalid password'}
+                return done(null, info); // create the loginMessage and save it to session as flashdata
+            }
             // all is well, return successful user
-            let token = jwt.sign({id:user.id},'server secret');
-
-            return done(null, false,req.flash('sesion',token));
+            return done(null, user);
             }).catch((err)=>{
                 console.log("Error:",err)
                 return done(null, false, {message: 'Something went wrong with your Signin'});
--- server/services/user/index.js
+++ server/services/user/index.js
@@ -2,27 +2,41 @@ const express = require('express')
 const passport = require('passport');
 const userController = require('../../controllers').singUp
 const jwt = require('jsonwebtoken');
+const config = require('../../config/config')
 
 
 const router = express.Router()
 // process the signup form
-router.post('/signup', passport.authenticate('local-signup', {
-    successRedirect: '/user/signup', // redirect to the secure profile section
-    failureRedirect: '/data', // redirect back to the signup page if there is an error
-    failureFlash: true // allow flash messages
-}));
-
-router.get('/signup', (req, res) => {
-     //var token = jwt.sign({id: newUser.id }, 'server secret');
-    //res.json(200,{token:token});
-    
+router.post('/signup', (req,res)=>{
+    passport.authenticate('local-signup', (err,user)=>{
+        if (user){
+            if(user.error){
+                console.log('user has error')
+                res.status(400).json(user)
+            }
+            const token = jwt.sign({id:user.id}, config.secret);
+            res.json({session:token})
+        }
+    })(req,res);
 })
 //login route
-router.post('/login', passport.authenticate('local-login',{
-    successFlash:true,
-    failureRedirect:'/data',
-    successRedirect:'/user/signup'
-}))
+router.post('/login', (req, res) => {
+    passport.authenticate('local-login',(err,user)=>{
+        if(user){
+            console.log("Test invalid password"+JSON.stringify(user));
+            if (user.error){
+                console.log('user test ' + JSON.stringify(user))
+                res.status(400).json(user)
+            }
+            const token = jwt.sign({id:user.id}, config.secret);
+            //res.status(200);
+            res.json({session:token})
+            
+
+        }
+    })(req,res);
+    
+});
 
 
 module.exports = router;
