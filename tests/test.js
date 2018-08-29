6013ba3e13bafadd900838f93c841353eecb3be9
Dennis <denniskamau3@gmail.com>
2018-08-29 20:37:11 +0300

    create travis test database

--- tests/test.js
+++ tests/test.js
@@ -89,9 +89,9 @@ describe('/GET data', ()=>{
 
 describe('/POST /user/signup', ()=>{
 
-     beforeEach(() => {
-          truncate();
-     });
+    //  beforeEach(() => {
+    //       truncate();
+    //  });
 
     it('it should return session token when user is registerd',(done)=>{
        
