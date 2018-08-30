12f3a05dae46344bc1c1c9eb9510f07705403605
Dennis <denniskamau3@gmail.com>
2018-08-30 23:37:35 +0300

    add mock database

---
+++ server/models/userMock.js
@@ -0,0 +1,24 @@
+const SequelizeMock = require('sequelize-mock');
+
+const dbMock = new SequelizeMock();
+
+module.exports = (DataTypes) => {
+    var UserMock = dbMock.define('user', {
+    id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {});
+    return UserMock
+}
