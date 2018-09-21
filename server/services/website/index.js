8eb6270b10fe2a06c06267b110c44f2477353620
Dennis <denniskamau3@gmail.com>
2018-09-21 18:15:24 +0300

    add website endpoint template

--- server/services/website/index.js
+++ server/services/website/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/add',(req,res)=>{
-    console.log('/add reached',req.body.data)
+    console.log('/add reached',req.body)
+    res.status(200).send({
+        body:req.body
+    })
 })
 
 router.get('/list', (req,res)=>{
