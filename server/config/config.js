b4cd3c075a9da71bee2b39c5b348be7545d10a8c
Dennis <denniskamau3@gmail.com>
2018-11-27 15:36:49 +0300

    Add twilio intergragion

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    'secret': 'hackerbayuniversity'
+    'secret': 'hackerbayuniversity',
+    'TWILIO_ACCOUNT_SID': 'AC97a269f8958f84cc626f50c52b910656',
+    'TWILIO_AUTH_TOKEN':'32c00486d764226dedb2b0bda647146b'
 }
