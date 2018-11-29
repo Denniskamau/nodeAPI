3e63df1b920048c70a1a24ae7009518c9a48e3b3
Dennis <denniskamau3@gmail.com>
2018-11-29 16:04:51 +0300

    add workers test

--- server/migrations/20181127134500-add-phone-number.js
+++ server/migrations/20181127134500-add-phone-number.js
@@ -9,7 +9,7 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
-   queryInterface.addColumn(
+    return queryInterface.addColumn(
      'Users',
      'phoneNo',
      Sequelize.INTEGER
