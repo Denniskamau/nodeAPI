60643cc65debd7ae3ccb03a5565d27f3f45aa5f9
Dennis Kamau <denniskamau3@gmail.com>
2019-06-22 21:18:13 +0300

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'secret': 'hackerbayuniversity',
-    'TWILIO_ACCOUNT_SID': 'AC97a269f8958f84cc626f50c52b910656',
-    'TWILIO_AUTH_TOKEN':'32c00486d764226dedb2b0bda647146b'
+    'secret': 'randomstring',
+    'TWILIO_ACCOUNT_SID': 'Your twilio ssid',
+    'TWILIO_AUTH_TOKEN':'Your twilio auth token'
 }
